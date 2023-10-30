e4189036c656bb3bec1b7f9bb4ec4bc5cbdd3b57
ArielAricoma <ariel16ary@gmail.com>
2023-10-29 22:30:31 -0300

    chi

--- Js/carrucel.js
+++ Js/carrucel.js
@@ -1,33 +1,32 @@
-var imagenes= ['../imagenes/De-gamer-a-soldado.jpg','../imagenes/El-multimillonario-disfrazado-contraataca.jpg','../imagenes/La-leyenda-del-joven-maestro.jpg','../imagenes/Mi-esposa-es-millonaria.jpg'];
-cont=0;
+var imagenes = ['../imagenes/De-gamer-a-soldado.jpg',
+    '../imagenes/El-multimillonario-disfrazado-contraataca.jpg',
+    '../imagenes/La-leyenda-del-joven-maestro.jpg',
+    '../imagenes/Mi-esposa-es-millonaria.jpg'];
 
-function carrucel(contenedor){
-    contenedor.addEventListener('click',e =>{
-        let atras=contenedor.querySelector('.atras'),
-        siguiente=contenedor.querySelector('.siguiente'),
-        img=contenedor.querySelector('.img'),
-        tgt=e.target;
-
-        if(tgt===atras){
+document.addEventListener("DOMContentLoaded", (event) => {
+    let cont = 0;
+    let contenedor = document.querySelector('.contenedor')
+    contenedor.addEventListener('click', e => {
+        let atras = contenedor.querySelector('.atras'),
+            siguiente = contenedor.querySelector('.siguiente'),
+            img = contenedor.querySelector('img'),
+            tgt = e.target;
+        if (tgt === atras) {
             if (cont > 0) {
-                img.src=imagenes[cont -1];
+                img.src = imagenes[cont - 1];
                 cont--;
-            }else{
-                img.src=imagenes[imagenes.length -1];
-                cont = imagenes.length-1;
+            } else {
+                img.src = imagenes[imagenes.length - 1];
+                cont = imagenes.length - 1;
             }
-        }else if(tgt===siguiente){
-            if (cont < imagenes.length) {
-                img.src=imagenes[cont +1];
+        } else if (tgt === siguiente) {
+            if (cont + 1 < imagenes.length) {
+                img.src = imagenes[cont + 1];
                 cont++;
-            }else{
-                img.src=imagenes[0];
+            } else {
+                img.src = imagenes[0];
                 cont = 0;
             }
         }
     });
-}
-document.addEventListener("DOMcontentLoaded",()=>{
-    let contenedor =document.querySelector('.contenedor')
-    carrucel(contenedor);
 });
